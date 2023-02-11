46b9478fcf8f434c1b5f52d7bd70ff822005ac4d
rajivjajo <rajivjajodia82552@gmail.com>
2023-02-11 18:08:23 +0530

    Test-case 1.1 for MoodAnalyser

---
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class MoodAnalyserTest {
+
+    static MoodAnalyser moodAnalyser = new MoodAnalyser();
+    @Test
+    public void whenGivenMoodShouldReturnSAD(){
+        String mood = moodAnalyser.analyseMood("i am in Sad Mood");
+        Assertions.assertEquals("SAD", mood);
+    }
+}
