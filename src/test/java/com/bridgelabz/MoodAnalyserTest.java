1a20a6f4298a81eef611e2e0be8af464a8f15f54
rajivjajo <rajivjajodia82552@gmail.com>
2023-02-11 18:58:34 +0530

    Refactor for MoodAnalyser

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -7,16 +7,9 @@ import org.junit.jupiter.api.Test;
 public class MoodAnalyserTest {
 
     static MoodAnalyser moodAnalyser = new MoodAnalyser();
-
     @Test
-    public void whenGivenMoodShouldReturnSAD() {
+    public void whenGivenMoodShouldReturnSAD(){
         String mood = moodAnalyser.analyseMood("I am in Sad Mood");
         Assertions.assertEquals("SAD", mood);
-    }
-
-    @Test
-    public void whenGivenMoodShouldReturnHappy() {
-        String mood = moodAnalyser.analyseMood("I am in Happy Mood");
-        Assertions.assertEquals("HAPPY", mood);
     }
 }
